5e48d1181be59676923393f0f7b80bfa817aec40
LiusCraft <meliuscraft@gmail.com>
2020-02-10 22:08:10 +0800

    Android-SocketDemo Code

---
+++ app/src/main/java/com/ilius/socketdemo/MainActivity.java
@@ -0,0 +1,123 @@
+package com.ilius.socketdemo;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.InetSocketAddress;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_send;
+    private EditText et_message;
+    private TextView tv_message;
+    private String nickName;
+    private SocketClient socketClient;
+
+    /**
+     * 处理Socket消息
+     */
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1: // 聊天消息
+                    tv_message.append(msg.obj.toString());
+                    break;
+                case 2: // 连接服务端成功，设置聊天昵称
+                    setNickName();
+                    break;
+                case -1: // socket处理出现异常(断开连接等异常...)
+                    Toast.makeText(MainActivity.this, msg.obj.toString(), Toast.LENGTH_LONG).show();
+                    break;
+                case -2:
+                    finish();
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setViewModule(); // 设置视图的组件
+        setListener(); // 设置设置监听事件
+        // 给socket信息与启动socket线程
+        socketClient = new SocketClient("192.168.1.5", 19730, handler);
+        Thread thread = new Thread(socketClient);
+        thread.start();
+    }
+
+    private void setViewModule(){
+        btn_send = findViewById(R.id.btn_send);
+        et_message = findViewById(R.id.et_message);
+        tv_message = findViewById(R.id.tv_message);
+    }
+
+    private void setListener() {
+        OnClick onClick = new OnClick();
+        btn_send.setOnClickListener(onClick);
+    }
+
+    private class OnClick implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_send:
+                    if (socketClient.isEnable()){
+                        socketClient.sendMessage(et_message.getText().toString());
+                        et_message.setText("");
+                    }else{
+                        Toast.makeText(MainActivity.this,"未连接到服务器...",Toast.LENGTH_LONG).show();
+                    }
+            }
+        }
+    }
+
+    private void setNickName() {
+        View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.dialog_setname, null);
+        AlertDialog.Builder setName = new AlertDialog.Builder(MainActivity.this);
+        final AlertDialog dialog = setName.create();
+        dialog.setView(view);
+        dialog.show();
+        dialog.setCancelable(false);
+        Button btn_start = view.findViewById(R.id.btn_start);
+        Button btn_clean = view.findViewById(R.id.btn_clean);
+        final EditText et_nickname = view.findViewById(R.id.et_nickname);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nickName = et_nickname.getText().toString();
+                if ("".equals(nickName)) {
+                    Toast.makeText(MainActivity.this, "请给自己一个昵称啊~", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                socketClient.sendMessage(nickName);
+                Toast.makeText(MainActivity.this, "您好!" + nickName, Toast.LENGTH_LONG).show();
+                dialog.dismiss();
+            }
+        });
+        btn_clean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/ilius/socketdemo/SocketClient.java
@@ -0,0 +1,120 @@
+package com.ilius.socketdemo;
+
+import android.os.Message;
+import android.util.Log;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class SocketClient implements Runnable{
+    private Socket socket;
+    private SocketAddress socketAddress;
+    private PrintWriter pw;
+    private Message message;
+    private android.os.Handler handler;
+
+    public SocketClient(String host, int port, android.os.Handler handler){
+        this.handler = handler; // MainActivity 的 handler
+        socketAddress = new InetSocketAddress(host,port); // 连接的地址+端口
+        this.socket = new Socket();
+    }
+
+    @Override
+    public void run() {
+        int resetCount = 4;
+            while (--resetCount>0){ // 这里给3次重连机会(在连接条件成立下)
+                try {
+                    // 给个连接延时(3000后还在连接则判断连接失败！)
+                    socket.connect(socketAddress,3000);
+                    pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(),"UTF-8"),true);
+                    resetCount = 4; // 连接成功了就再给3次重连机会
+                    if (!this.readMessage()){ // 这里进入读socket消息的方法,如果出现异常则返回false
+                        continue; // 这里直接进入下个循环...目前这的代码块啥用没有，但以后我需要用到。
+                    }
+                } catch (Exception e) { // 连接失败用handler发送消息处理。
+                    Log.d("-e",e.toString());
+                    socket = new Socket();
+                    message = new Message();
+                    message.what = -1;
+                    message.obj = "尝试重写连接服务器..."+resetCount;
+                    handler.sendMessage(message);
+                }
+            }
+            message = new Message();
+            message.what = -2;
+            handler.sendMessage(message); // 退出程序...
+    }
+
+    /**
+     * 读Socket消息...需先连接服务端
+     * @return 读取消息失败则返回 false
+     */
+    private Boolean readMessage(){
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(),"UTF-8"));
+            message = new Message();
+            message.what=2;
+            message.obj="连接成功，请设置昵称！";
+            handler.sendMessage(message);  // 让安卓弹出设置聊天昵称的窗口
+            while (isEnable()){ // socket为连接状态下则循环
+                char messages[] = new char[1024];
+                int i = br.read(messages); // 读消息（每次读1024个字符）
+                if (i>-1){
+                    char str0[] =new char[i];
+                    System.arraycopy(messages, 0, str0, 0, i);
+                    String str = new String(str0); // 转成字符串
+                    message = new Message();
+                    message.what = 1;
+                    message.obj = str;
+                    handler.sendMessage(message); // 让安卓把收到的聊天消息加入到消息栏中...
+                }else {
+                    break;
+                }
+            }
+        }catch(Exception e){
+
+            message = new Message();
+            message.what = -1;
+            message.obj = "服务器已断开...正在尝试重连";
+            handler.sendMessage(message);
+
+        }
+        return false;
+    }
+
+    /**
+     * Socket发送消息
+     * @param message 发送的内容
+     * @return 发送成功为True 否则 False
+     */
+    public Boolean sendMessage(final String message){
+        if (this.isEnable()){ // 连接状态为真，才可以发送消息
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    // 很骚气，这个也必须用线程,不然就报错...哎
+                    // 因为发送消息也是网络请求操作，不这样搞会出现卡死。
+                    pw.println(message);
+                }
+            }).start();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 返回Socket是否已连接
+     * @return 如果socket已连接返回 True 否则 False
+     */
+    public Boolean isEnable(){
+        // 下面的逻辑代码是 是否连接过...并且是否不是关闭状态，才能给出是否已连接...
+        if(this.socket.isConnected()&&!this.socket.isClosed()){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+}
